66870b77d94f3f71ffa43ab4c906a384d2dcf141
Lp2000 <perhot.leon@gmail.com>
2017-11-16 09:57:28 +0100

    Rezultat bele

--- program.cpp
+++ program.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main ()
 {
+    int mi[20], vi[20];
+    cout<<"1.Upisite broj bodove ekipe mi"<<endl;
+    cout<<"2.Upisite brij bodove ekipe vi"<<endl;
+    cout<<"Unesite 1. ili 2."<<endl;
+    int izbor;
+    cin>>izbor;
+    int brIgre = 0;
+    if(izbor==1)
+    {
+        cout<<"Upisite bodove za ekipu mi"<<endl;
+        cin >> mi[brIgre];
+        vi[brIgre] = 162 - mi [brIgre];
+    }
+    else if(izbor==2)
+    {
+        cout<<"Upisite bodove za ekipu vi"<<endl;
+        cin >> v[brIgre];
+    }
     return 0;
 }
